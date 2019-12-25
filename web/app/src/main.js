e94f00565eaafbaab20a7a83a912b2406b8411cd
Zogoo <chtsogbadrakh@gmail.com>
2019-12-25 21:41:48 +0900

    Only validate when value exist

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -26,9 +26,9 @@ Vue.prototype.$auth = {
   },
   getAccessToken() {
     let storedToken = localStorage.getItem(this.accessTokenField);
-    this.parseJwt(storedToken);
-
+    
     if (storedToken != null) {
+      this.parseJwt(storedToken);
       return storedToken;
     } else {
       return null
